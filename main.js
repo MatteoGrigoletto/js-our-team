7684dfe6c1f1544b51939d3656a1c5afc7cadda4
MatteoGrigoletto <mttgrigoletto@gmail.com>
2022-11-07 14:43:58 +0100

    Aggiunge lista oggetti e immagini

--- main.js
+++ main.js
@@ -1,2 +1,35 @@
 `use strict`;
 
+let team = [
+    {
+        name: `Wayne Barnett`,
+        job: `Founder Editor`,
+        image: `img/wayne-barnett-founder-ceo.jpg`
+    },
+    {
+        name: `Angela Caroll`,
+        job: `Chief Editor`,
+        image: `img/angela-caroll-chief-editor.jpg`
+    },
+    {
+        name: `Walter Gordon`,
+        job: `Office Manager`,
+        image: `img/walter-gordon-office-manager.jpg`
+    },
+    {
+        name: `Angela Lopez`,
+        job: `Social Media Manager`,
+        image: `img/angela-lopez-social-media-manager.jpg`
+    },
+    {
+        name: `Scott Estrada`,
+        job: `Developer`,
+        image: `img/scott-estrada-developer.jpg`
+    },
+    {
+        name: `Barbara Ramos`,
+        job: `Graphic Designer`,
+        image: `img/barbara-ramos-graphic-designer.jpg`
+    }
+]
+
